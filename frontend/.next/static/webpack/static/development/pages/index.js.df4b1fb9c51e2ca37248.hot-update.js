8bc5fe01ce4768740a0df537dea1a6ab6a7cb565
tiagosalema <tiagof.salema@gmail.com>
2019-02-28 17:38:43 +0000

    included some necessary files

---
+++ frontend/.next/static/webpack/static/development/pages/index.js.df4b1fb9c51e2ca37248.hot-update.js
@@ -0,0 +1,46 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/Form.jsx":
+/*!*****************************!*\
+  !*** ./components/Form.jsx ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+
+
+/***/ }),
+
+/***/ "./pages/index.jsx":
+/*!*************************!*\
+  !*** ./pages/index.jsx ***!
+  \*************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Form__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../components/Form */ "./components/Form.jsx");
+/* harmony import */ var _components_Form__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_components_Form__WEBPACK_IMPORTED_MODULE_2__);
+var _jsxFileName = "C:\\Users\\tiago\\OneDrive\\Documents\\_Projects\\app\\frontend\\pages\\index.jsx";
+
+
+
+var Home = function Home() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "This is the home page, my dude.");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.df4b1fb9c51e2ca37248.hot-update.js.map
