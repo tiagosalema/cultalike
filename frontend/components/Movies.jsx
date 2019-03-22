4cba4219e4ec64fcbe77ca41e272c82f3cb8f912
tiagosalema <tiagof.salema@gmail.com>
2019-03-22 21:54:37 +0000

    Display rated movies in Movies page

--- frontend/components/Movies.jsx
+++ frontend/components/Movies.jsx
@@ -1,11 +1,13 @@
 import { Component } from "react";
 import styled from "styled-components";
 import Head from "next/head";
+import { Query } from "react-apollo";
 
 import { getMovies } from "../lib/fetchMovies";
 
 import RateMovie from "./Mutations/RateMovie";
 import MovieInput from "./MovieInput";
+import { ALL_REVIEWED_MOVIES_QUERY } from "./ReviewedMovies";
 
 const imgSize = 300;
 
@@ -48,40 +50,49 @@ class Movies extends Component {
           {!this.state.movies.length ? (
             <p>Loading...</p>
           ) : (
-            this.state.movies.map(movie => {
-              const { title } = movie;
-              return (
-                <div key={movie.id}>
-                  <div className="container">
-                    <img
-                      src={"https://image.tmdb.org/t/p/w" + imgSize + movie.poster_path}
-                      alt={title}
-                      style={{ width: "100%" }}
-                    />
-                    <div className="centered">
-                      {0 ? (
-                        <div style={{ backgroundColor: "green", padding: "10px" }}>
-                          Rated!
+            <Query query={ALL_REVIEWED_MOVIES_QUERY}>
+              {({ data, loading }) => {
+                return this.state.movies.map(movie => {
+                  const { title } = movie;
+                  return (
+                    <div key={movie.id}>
+                      <div className="container">
+                        <img
+                          src={
+                            "https://image.tmdb.org/t/p/w" + imgSize + movie.poster_path
+                          }
+                          alt={title}
+                          style={{ width: "100%" }}
+                        />
+                        <div className="centered">
+                          {!loading &&
+                          data.ratedMovies.some(movie => movie.movie === title) ? (
+                            <div style={{ backgroundColor: "green", padding: "10px" }}>
+                              Rated!
+                            </div>
+                          ) : (
+                            <RateMovie
+                              inputs={{ title, rate: this.state.rateMovie[title] }}
+                            >
+                              <label>
+                                Rate:
+                                <MovieInput
+                                  title={title}
+                                  value={this.state.rateMovie[title]}
+                                  onChange={this.handleChange}
+                                  onSubmit={this.handleSubmit}
+                                />
+                              </label>
+                            </RateMovie>
+                          )}
                         </div>
-                      ) : (
-                        <RateMovie inputs={{ title, rate: this.state.rateMovie[title] }}>
-                          <label>
-                            Rate:
-                            <MovieInput
-                              title={title}
-                              value={this.state.rateMovie[title]}
-                              onChange={this.handleChange}
-                              onSubmit={this.handleSubmit}
-                            />
-                          </label>
-                        </RateMovie>
-                      )}
+                      </div>
+                      <p>{title}</p>
                     </div>
-                  </div>
-                  <p>{title}</p>
-                </div>
-              );
-            })
+                  );
+                });
+              }}
+            </Query>
           )}
         </ItemsList>
       </Center>
